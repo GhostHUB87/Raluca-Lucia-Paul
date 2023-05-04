65b962ea1645406070806da3cd41b4e11c67b0dd
GhostHUB87 <delcea_sergiu@yahoo.com>
2023-05-04 15:59:03 +0300

    Add files via upload

---
+++ GSAP.js
@@ -0,0 +1,14 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".left-main", {
+//     scrollTrigger:{
+//         trigger: ".test",
+//         start: "-1101px 10px",
+//         end: "bottom top",
+//         markers: true,        
+//     },
+//     backgroundImage : "url('images/LAW.JPG')",
+//     backgroundSize: "contain",
+//    duration: 3,
+   
+// })
